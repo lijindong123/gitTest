245b142bd5f537630fd7cd9527d4e91a9881449c
lijindong123 <55774005+lijindong123@users.noreply.github.com>
2021-06-22 22:19:12 +0800

    Update TttClz.java

--- src/test/java/useTest/TttClz.java
+++ src/test/java/useTest/TttClz.java
@@ -7,6 +7,7 @@ public class TttClz {
 	UserTest uu=new UserTest();
 	private String name;
 	public String getName() {
+		System.out.println("write by github");
 		return name;
 	}
 	public void setName(String name) {
