4a54a889e20da20d14a0c8cd56586005743c4c08
lijindong123 <2049915725@qq.com>
2021-06-26 14:43:00 +0800

    add one line

--- src/main/java/justTest/UserTest.java
+++ src/main/java/justTest/UserTest.java
@@ -5,7 +5,8 @@ public class UserTest {
 	private String name;//edit by lee
 	public static void main(String[] args) {
 		
-		
+		  
+		System.out.println("create by redMi");
 		System.out.println("create by redMi");
 	}
 	
