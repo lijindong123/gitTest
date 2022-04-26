a4d4a19fbd95c20bd241cbc3e2ec4d24b18ff737
lijindong123 <55774005+lijindong123@users.noreply.github.com>
2022-04-26 15:22:11 +0800

    Update UserTest.java

--- src/main/java/justTest/UserTest.java
+++ src/main/java/justTest/UserTest.java
@@ -5,7 +5,7 @@ public class UserTest {
 	private String name;//edit by lee
 	public static void main(String[] args) {
 		
-		  
+		//go
 		System.out.println("create by redMi");
 		System.out.println("create by redMi");
 	}
